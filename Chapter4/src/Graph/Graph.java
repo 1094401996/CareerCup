f10a2f56f42815d9a6264473e1e87080389fa4bc
gengchen <gengchen@ubuntu.ubuntu-domain>
2013-03-24 17:07:33 -0700

    finish graph

--- Chapter4/src/Graph/Graph.java
+++ Chapter4/src/Graph/Graph.java
@@ -7,19 +7,18 @@ public class Graph {
 	private final int V;
 	private int E;
 	private LinkedList <Integer>[] adj;
-	
-	public Graph(int V){
-		this.V = V;
-		adj = (LinkedList<Integer>[])new LinkedList[V];//attention
-		for(int v = 0; v < V; v++){
-			adj[v] = new LinkedList<Integer>();
-		}
-	}
-	public Graph(int[]data) throws Exception{//length should be 2E+2
+
+	@SuppressWarnings("unchecked")
+	public Graph(int[]data) throws Exception{   //length should be 2E+2
 		if(data.length >=2){
 			this.V = data[0];
 			this.E = data[1];
 			if(data.length == 2*E + 2){
+				adj = (LinkedList<Integer>[])new LinkedList[V];//attention,you could build generic array directly with new keyword
+				for(int v = 0; v < V; v++){
+					adj[v] = new LinkedList<Integer>();
+				}
+
 				for (int i = 2; i < data.length;){
 					int v = data[i++];
 					int w = data[i++];
